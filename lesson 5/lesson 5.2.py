64e5d36cec40acd373b3d0345994af724e22961c
alaska1011 <igord1707@gmail.com>
2025-04-26 19:39:21 +0300

    10

---
+++ lesson 5/lesson 5.2.py
@@ -0,0 +1,31 @@
+while True:
+    print("Enter first number: ")
+    num1 = float(input())
+
+    print("Enter operation (+, -, *, /): ")
+    operation = input()
+
+    print("Enter second number: ")
+    num2 = float(input())
+
+    if operation == '+':
+        result = num1 + num2
+        print("result:", result)
+    elif operation == '-':
+        result = num1 - num2
+        print("result:", result)
+    elif operation == '*':
+        result = num1 * num2
+        print("result:", result)
+    elif operation == '/':
+        if num2 == 0:
+            print("Error Divisor cannot be equal to 0")
+        else:
+            result = num1 / num2
+            print("result:", result)
+    else:
+        print("Error Invalid operation! Try again")
+    continue_calculation = input("Enter y").strip().lower()
+    if continue_calculation not in ['y']:
+        break
+
