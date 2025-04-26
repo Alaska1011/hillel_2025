7cd57460c48092f009a63618a510d3dff53e8e20
alaska1011 <igord1707@gmail.com>
2025-04-26 19:01:19 +0300

    Initial commit

--- lesson 4/lesson 4.1.py
+++ lesson 4/lesson 4.1.py
@@ -1,4 +1,4 @@
-чlst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 result = [x for x in lst if x != 0]
 result.extend([0] * (len(lst) - len(result)))
 print(result)
